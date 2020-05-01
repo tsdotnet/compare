7f035c2e02967a6816517a9ae52b945be7669db7
Oren (electricessence) <oren@electrified.net>
2020-05-01 16:17:20 -0700

    Added more comparison types.

--- dist/Comparison.d.ts
+++ dist/Comparison.d.ts
@@ -3,14 +3,20 @@
  * Licensing: MIT
  */
 
-export interface Comparison<T> {
+export interface Comparison<T>
+{
 	(a: T, b: T): number;
+
 	(a: T, b: T, strict: boolean): number;
+
 	(a: T, b: T, strict?: boolean): number;
 }
 
-export interface EqualityComparison<T> {
+export interface EqualityComparison<T>
+{
 	(a: T, b: T): boolean;
+
 	(a: T, b: T, strict: boolean): boolean;
+
 	(a: T, b: T, strict?: boolean): boolean;
 }
