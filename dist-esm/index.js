a380f639b49e659fa21ec9f0784ef5c96222006b
Oren (electricessence) <oren@electrified.net>
2020-05-02 13:08:27 -0700

    Fixing rename issue.

--- dist-esm/index.js
+++
@@ -1,6 +0,0 @@
-import { areEqual, areEquivalent, compare } from './compare';
-import type from './type';
-import SortContext from './SortContext';
-import KeySortedContext from './KeySortedContext';
-export { areEqual, areEquivalent, compare, type, SortContext, KeySortedContext };
-//# sourceMappingURL=index.js.map
